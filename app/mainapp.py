5b701c7f6b66fd763047e32135e2c489ec8d9848
Muhumuza Deus <157320582+MDeus-ai@users.noreply.github.com>
2024-04-01 13:00:07 +0300

    🦀

---
+++ app/mainapp.py
@@ -0,0 +1,8 @@
+from src.predict import model_predict
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+@app.route('/')
+
+def home():
+    return render_template("home.html")
